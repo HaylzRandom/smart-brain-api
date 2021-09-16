fc6c0f7b7c8b410ec1368ec9d8daf1c70013c769
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2021-09-16 21:41:25 +0100

    Adding db connections

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ import { handleApiCall, handleImage } from './components/image.js';
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: process.env.DB_HOST,
+		host: process.env.DATABASE_URL,
 		port: process.env.DB_PORT,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME,
+		ssl: true,
 	},
 });
 

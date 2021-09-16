c78cca41f0407a18f920a8e44bbf28564d78fae7
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2021-09-16 21:46:16 +0100

    Adding db connections

--- server.js
+++ server.js
@@ -14,11 +14,9 @@ const db = knex({
 	client: 'pg',
 	connection: {
 		connectionString: process.env.DATABASE_URL,
-		/* 		port: process.env.DB_PORT,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME, */
-		ssl: true,
+		ssl: {
+			rejectUnauthorized: false,
+		},
 	},
 });
 
